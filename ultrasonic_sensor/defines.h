6298ed5733c181f08bd4a7d97458ed19de4f5648
Andrew McOlash <amcolash@gmail.com>
2017-01-28 01:43:30 -0600

    Add in new project using ultrasonic sensor

---
+++ ultrasonic_sensor/defines.h
@@ -0,0 +1,65 @@
+/* 
+ * File:   defines.h
+ * Author: andrew
+ *
+ * Created on December 25, 2016, 7:55 PM
+ */
+
+#ifndef DEFINES_H
+#define	DEFINES_H
+
+
+// INCLUDES --------------------------------------------------------------------
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// CONFIGURATION BITS ----------------------------------------------------------
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover Mode (Internal/External Switchover Mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config CPUDIV = NOCLKDIV// CPU System Clock Selection Bit (NO CPU system divide)
+#pragma config USBLSCLK = 48MHz // USB Low SPeed Clock Selection bit (System clock expects 48 MHz, FS/LS USB CLKENs divide-by is set to 8.)
+#pragma config PLLMULT = 4x     // PLL Multipler Selection Bit (4x Output Frequency Selected)
+#pragma config PLLEN = DISABLED // PLL Enable Bit (3x or 4x PLL Disabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+
+// DEFINES ---------------------------------------------------------------------
+
+#define _XTAL_FREQ  500000
+
+#define OUTPUT      0
+#define INPUT       1
+
+#define true        1
+#define false       0
+
+// GPIO
+#define RC0_TRIS    TRISCbits.TRISC0
+#define RC0         LATCbits.LATC0
+
+#define RC3_TRIS    TRISCbits.TRISC3
+#define RC3         LATCbits.LATC3
+
+#define RC4_TRIS    TRISCbits.TRISC4
+#define RC4         PORTCbits.RC4
+
+#endif	/* DEFINES_H */
---
+++ ultrasonic_sensor/main.c
@@ -0,0 +1,57 @@
+/* 
+ * File:   main.c
+ * Author: Andrew McOlash
+ *
+ * Created on December 25, 2016, 7:54 PM
+ */
+
+#include "defines.h"
+
+// FUNCTION PROTOTYPES ---------------------------------------------------------
+
+void gpio_init();
+
+// MAIN FUNCTION ---------------------------------------------------------------
+
+void main() {
+    gpio_init();
+    
+    // Turn off LED at start
+    RC0 = false;
+    
+    while (1) {        
+        RC3 = true;
+        __delay_us(50);
+        RC3 = false;
+        
+        int time = 0;
+        
+        while (RC4 == false) {
+            __delay_us(1);
+        }
+        
+        while (RC4 == true) {
+            time++;
+            __delay_us(1);
+        }
+        
+        // 1.118
+        RC0 = (time * 1.111) > 30; // Hacked number because of clock speed I set (I think). Anyways, seems to be within 0.25cm at 60cm
+        
+//        RC0 = (time * 1.7150) > 25; // LED on if more than 25cm away - actual distance was ~ 16cm
+        
+        __delay_ms(2000);
+    }
+}
+
+// FUNCTION IMPLEMENTATIONS ----------------------------------------------------
+
+void gpio_init() {
+    // RC_0 = LED1
+    // RC_3 = Ultrasonic Sensor - Trigger (Output)
+    // RC_4 = Ultrasonic Sensor - Echo (Input)
+    
+    RC0_TRIS = OUTPUT;
+    RC3_TRIS = OUTPUT;
+    RC4_TRIS = INPUT;
+}
